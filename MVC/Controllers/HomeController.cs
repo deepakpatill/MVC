95153baa519e820b3b5d148723c99c2a656e443d
Deepak Patil <deepak.patil@atriina.com>
2023-01-23 16:10:14 +0530

    Inital commit

---
+++ MVC/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC.Models;
+
+namespace MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return "Controller Demo";
+        }
+
+        public ActionResult ViewDemo()
+        {
+            ViewBag.Message = "View Demo";
+            ViewBag.Countries = new List<string>()
+            {
+                "India",
+                "US",
+                "Germany"
+            };
+
+            return View();
+        }
+
+        public ActionResult ModelDemo()
+        {
+            ViewBag.Message = "Model Demo";
+            Employee E = new Employee()
+            {
+                EmployeeId = 101,
+                name = "ABC" 
+            };
+
+
+            return View(E);
+        }
+    }
+}
